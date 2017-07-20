2018e1d660dc2061d441f83997b5786f685b5205
Bartcardi <Bartcardi@users.noreply.github.com>
2017-07-20 10:56:51 +0200

    Updated note 'group 5'

---
+++ group 5.py
@@ -0,0 +1,35 @@
+a shift in a business model
+
+why new business model, and for who?? -> for us
+
+action recognition for violence
+
+looking for violence before it happens???
+
+
+Final:
+
+literature review, for arguments
+
+MoSift for movement/motion modelling
+
+90% acc agression detection
+
+RQ:
+
+2) 1) feasibility detecting violence with FD
+2) valuable for stadiums?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
